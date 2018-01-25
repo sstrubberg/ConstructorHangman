dfb8b23e23ddfc27cb83a0a6fb3ab33cc2ab6dbe
Scott <sstrubberg@us.ibm.com>
2018-01-25 15:55:05 -0600

    not much here

---
+++ WordToYourMother.js
@@ -0,0 +1,12 @@
+var startingLetters = "abcdefghijklmnopqrstuvwxyz";
+var startingLives = require("./hangman.js");
+
+var Words = function(listOfWords) {
+    this.listOfWords = listOfWords;
+    console.log(this.listOfWords);
+}
+
+// var readWords = new Words("Scott");
+// console.log(readWords.listOfWords);
+
+
---
+++ hangman.js
@@ -0,0 +1,7 @@
+var startingLives = 10;
+module.exports = startingLives;
+
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+var theWords = ["Kara", "Parker", "Scott", "Gail", "John", "Jackie"];
